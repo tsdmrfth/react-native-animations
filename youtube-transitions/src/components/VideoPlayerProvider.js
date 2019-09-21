60d4a1e237fa9fa13f3d5db4d54b813024a36c2b
Fatih Tasdemir <tsdmrfth@gmail.com>
2019-09-21 18:53:45 +0300

    Added VideoPlayerProvider.

--- youtube-transitions/src/components/VideoPlayerProvider.js
+++ youtube-transitions/src/components/VideoPlayerProvider.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import VideoPlayerContext from './VideoPlayerContext';
 
-export default class VideoPlayerProvider extends React.Component {
+export default class VideoPlayerProvider extends React.PureComponent {
+
+    state = {
+        video: null
+    }
+
+    setVideo = video => this.setState({ video })
 
     render() {
-        return (
-            <VideoPlayerContext.Provider>
+        const { setVideo } = this
+        const { children } = this.props
+        const { video } = this.state
 
+        return (
+            <VideoPlayerContext.Provider value={{ video, setVideo }}>
+                {children}
             </VideoPlayerContext.Provider>
         )
     }
