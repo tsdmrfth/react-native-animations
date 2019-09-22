60a98162ebd3f90fe3f178424aab554900ac4e16
Fatih Tasdemir <tsdmrfth@gmail.com>
2019-09-22 09:27:27 +0300

    Added translation animations to VideoDetail.

--- youtube-transitions/src/components/VideoDetail.js
+++ youtube-transitions/src/components/VideoDetail.js
@@ -1,26 +1,97 @@
 import React from 'react';
-import { PanGestureHandler } from 'react-native-gesture-handler';
+import { Dimensions } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 import Animated from 'react-native-reanimated';
+
+const { height } = Dimensions.get('window')
 
 const {
     View: AnimatedView,
     event,
-    Value
+    Value,
+    cond,
+    eq,
+    set,
+    Clock,
+    add,
+    lessThan,
+    clockRunning,
+    spring,
+    stopClock,
+    startClock,
+    debug
 } = Animated
+const { END } = State
+
+function runSpring(clock, value, dest, velocity) {
+    const state = {
+        finished: new Value(0),
+        velocity: new Value(0),
+        position: new Value(0),
+        time: new Value(0),
+        velocity
+    }
+
+    const config = {
+        damping: 20,
+        mass: 1,
+        stiffness: 100,
+        overshootClamping: false,
+        restSpeedThreshold: 1,
+        restDisplacementThreshold: 0.5,
+        toValue: new Value(0),
+    }
+
+    return [
+        cond(clockRunning(clock), 0, [
+            set(state.finished, 0),
+            set(state.velocity, velocity),
+            set(state.position, value),
+            set(config.toValue, dest),
+            startClock(clock),
+        ]),
+        spring(clock, state, config),
+        cond(state.finished, stopClock(clock)),
+        state.position,
+    ]
+}
 
 export default class VideoDetail extends React.Component {
 
     constructor(props) {
         super(props)
-        this.translateY = new Value(0)
+        const translateY = new Value(0)
+        const offsetY = new Value(0)
+        const gestureState = new Value()
+        const velocityY = new Value(0)
+        const clock = new Clock()
+        const oneThirdOfScreenHeight = height / 3
+        const addY = add(translateY, offsetY)
+        const finalPoint = cond(lessThan(addY, oneThirdOfScreenHeight), 0, height - 100)
+
         this.onGestureEvent = event(
             [
                 {
                     nativeEvent: {
-                        translationY: this.translateY,
+                        translationY: translateY,
+                        state: gestureState,
+                        velocityY
                     },
                 }
-            ], { useNativeDriver: true })
+            ], { useNativeDriver: true }
+        )
+
+        this.translationY = cond(
+            eq(gestureState, END),
+            [
+                set(translateY, runSpring(clock, add(translateY, offsetY), finalPoint, velocityY)),
+                set(offsetY, translateY),
+                translateY
+            ],
+            [
+                add(offsetY, translateY)
+            ]
+        )
     }
 
     render() {
@@ -39,7 +110,7 @@ export default class VideoDetail extends React.Component {
                         alignItems: 'center',
                         alignSelf: 'center',
                         transform: [
-                            { translateY: this.translateY }
+                            { translateY: this.translationY }
                         ]
                     }}>
                 </AnimatedView>
